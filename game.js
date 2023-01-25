3d5a7d0aee9604f6e65a40d73278ad75f49b9c10
laura_z <llzerr@hotmail.com>
2023-01-25 16:51:04 -0700

    Create a new pattern

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
 // alert ("Hello, Welcome to the game!");
+const buttonColours = ["red", "blue", "green", "yellow"];
+const gamePattern =[];
+
+function nextSequence () {
+  let randomNumber = Math.floor(Math.random() * 4);
+  // console.log(randomNumber);
+  let randomChosenColour = buttonColours[randomNumber];
+  console.log(randomChosenColour);
+  gamePattern.push(randomChosenColour);
+  console.log(gamePattern);
+};
+
+// nextSequence();
