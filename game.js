e2e5a3e640f2d83f439426612c47403556242d86
laura_z <llzerr@hotmail.com>
2023-01-28 19:13:52 -0700

    Update to check which button user pressed

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 
 const buttonColours = ["red", "blue", "green", "yellow"];
 const gamePattern =[];
+const userClickedPattern =[];
 
 function nextSequence () {
   let randomNumber = Math.floor(Math.random() * 4);
@@ -13,3 +14,10 @@ function nextSequence () {
   const audio = new Audio(`sounds/${randomChosenColour}.mp3`);
   audio.play();
 };
+
+$(".btn").click(function () {
+  const userChosenColour = this.id;
+  console.log(userChosenColour);
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+});
