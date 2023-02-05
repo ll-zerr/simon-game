d2feb414f35b8600f14127bb26ddc2468c089553
laura_z <llzerr@hotmail.com>
2023-02-04 20:01:20 -0700

    Check user sequence against game pattern

--- game.js
+++ game.js
@@ -1,44 +1,46 @@
-
 const buttonColours = ["red", "blue", "green", "yellow"];
-const gamePattern =[];
-const userClickedPattern =[];
+const gamePattern = [];
+let userClickedPattern = [];
 let started = false;
 let level = 0;
 
 // start the game
-$(document).keydown(function (event) {
+$(document).keydown(function(event) {
   // console.log(event.key);
-if (!started) {
-  $("#level-title").text("Level " + level);
+  if (!started) {
+    $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
   }
 });
 
-
-function nextSequence () {
+//  create a game pattern
+function nextSequence() {
+  userClickedPattern = [];
   level++;
   $("#level-title").text("Level " + level);
   let randomNumber = Math.floor(Math.random() * 4);
   // console.log(randomNumber);
   let randomChosenColour = buttonColours[randomNumber];
-  console.log(randomChosenColour);
+  // console.log(randomChosenColour);
   gamePattern.push(randomChosenColour);
   console.log(gamePattern);
-  $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
 };
 
-$(".btn").click(function () {
+//  listen for user clicked pattern
+$(".btn").click(function() {
   const userChosenColour = this.id;
-  console.log(userChosenColour);
+  // console.log(userChosenColour);
   userClickedPattern.push(userChosenColour);
   console.log(userClickedPattern);
   playSound(userChosenColour);
   animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
-function playSound (name) {
+function playSound(name) {
   const audio = new Audio(`sounds/${name}.mp3`);
   audio.play();
 };
@@ -48,4 +50,18 @@ function animatePress(currentColour) {
   setTimeout(function() {
     $("#" + currentColour).removeClass("pressed");
   }, 100);
+};
+
+//  check user clicked pattern against game pattern
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    // console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+  }
 };
